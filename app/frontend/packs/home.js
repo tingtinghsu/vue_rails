f869fc6e3124be6c919e2c01ce0483d23ce1052f
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-09-28 00:16:31 +0800

    day15

--- app/frontend/packs/home.js
+++ app/frontend/packs/home.js
@@ -1,40 +1,37 @@
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import Foot from "../components/foot"
-import Counter from "../components/counter"
 import store from './store';
+import axios from 'axios-on-rails'
+
 
 Vue.use(TurbolinksAdapter)
 
 
 document.addEventListener('turbolinks:load', () => {
   let el = document.querySelector("#content");
+  const cors = 'https://cors-anywhere.herokuapp.com/';
+  const url = 'https://tinyfac.es/api/users';
 
   if (el){
     new Vue({
       store,
       el,
       data: {
-        day: "第 13 天",
-        topic: "Vuex: 狀態管理",
+        day: "第 15 天",
+        topic: "Vue instance的 mounting 階段 mounted()： el指定的標籤已經掛載完成",
+        audience: "訂閱這個鐵人賽系列文的觀眾：",
+        subscribers: []
       },
-      computed: {
-        count(){
-          return store.state.count
-        }
+      mounted() {
+        var vm = this
+        // axios.get('https://tinyfac.es/api/users')
+        axios.get(`${cors}${url}`)
+            .then((response) => {
+                vm.subscribers = response.data
+            })
       },
-      methods: {
-        increment(){
-          store.commit('increment')
-        },
-        decrement(){
-          store.commit('decrement')
-        }
-      },
-      components: { Counter,Foot }
+      components: { Foot }
     })
-
-
   }
-
 })
