2083f1df548c83caa1412e3569d3f08a1ad69748
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-10-01 09:52:14 +0800

    login then redirect to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]  
   def home
     
   end
   def index
+    if current_user
+      redirect_to dashboard_path
+
+    end    
+  end
+  def dashboard
     
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # root 'pages#home'
   root 'pages#index'
-
+  get "dashboard", to: "pages#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
