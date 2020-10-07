fdcd5b8aeb4bc30d653b1ce2c3c632a0f1912138
tingtinghsu <tingtinghsu.tw@gmail.com>
2020-10-07 22:07:56 +0800

    day23 vuex store column.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -10,11 +10,10 @@ import 'scripts'
 import 'styles'
 
 import Vue from "vue/dist/vue.esm";
-import Column from "components/kanban/column"
-import Rails from '@rails/ujs'
+import Column from "components/kanban/column";
 import draggable from 'vuedraggable';
 import store from 'stores/column';
-import { mapGetters, mapActions } from 'vuex';
+import { mapActions } from 'vuex';
 
 document.addEventListener("turbolinks:load", () => {
   let el = document.querySelector("#column");
@@ -28,52 +27,25 @@ document.addEventListener("turbolinks:load", () => {
         // columns: []
       // },
       computed: {
-        // 接getters，唯讀，觀察data屬性
-        ...mapGetters(["columns"])
+        // 接getters，唯讀，僅能觀察data屬性
+        // ...mapGetters(["columns"])
+        columns: {
+          get(){
+            return this.$store.state.columns
+          },
+
+          set(val){
+            this.$store.commit('UPDATE_COLUMNS', val)
+          }
+        }
       },
       components: { Column, draggable },
       methods: {
-        ...mapActions(["fetchColumn"]),     
-        dragColumn(evt){
-          console.log(evt)
-          // console.log(evt.moved)
-          // new一個FormData()物件叫做formData
-          let data = new FormData();
-          // acts as list 的 position：從1開始算，移動到新的位置       
-          data.append("column[position]", evt.moved.newIndex + 1)
-          console.log(data)
-
-          //  /kanbans/2/columns/1/drag(.:format)`
-          Rails.ajax({
-            url: `/kanbans/${this.kanban_id}/columns/${this.columns[evt.moved.newIndex].id}/drag`,
-            type: 'PUT',
-            data,
-            dataType: 'json',
-            success: result => {
-              console.log(result);
-            },
-            error: error => {
-              console.log(error);            
-            }
-          });
-        }
+        ...mapActions(["fetchColumn", "dragColumn"])
       },
       beforeMount(){
         // 呼叫資料      
         this.fetchColumn(el.dataset.kanbanid);
-        // Rails.ajax({
-        //   url: `/kanbans/${this.kanban_id}/columns.json`,
-        //   type: 'GET',
-        //   dataType: 'json',
-        //   success: result => {
-        //     // console.log(this.kanban_id);
-        //     // console.log(result);
-        //     this.columns = result;
-        //   },
-        //   error: error => {
-        //     console.log(error);            
-        //   }
-        // });
       }
     });
   }
--- app/frontend/stores/column.js
+++ app/frontend/stores/column.js
@@ -4,24 +4,40 @@ import Rails from '@rails/ujs';
 
 Vue.use(Vuex);
 
-// 定義一個新的 Vue Store
 export default new Vuex.Store({
   state: {
-    // columns, tickets：預設空陣列
     columns: []
   },
   getters:{
     columns: state => state.columns
   },
   mutations: {
-    // 將state設定為參數
-    // 請mutation來更新state
     UPDATE_COLUMNS(state, columns){
       state.columns = columns;
     }
   },
-  // 在外面的地方呼叫做事情
   actions: {
+    dragColumn({ commit, state }, evt) {
+        let data = new FormData();
+        let el = document.querySelector("#column");           
+        data.append("column[position]", evt.moved.newIndex + 1)
+        console.log(evt)
+        // console.log(`/kanbans/${el.dataset.kanbanid}/columns/${state.columns[evt.moved.newIndex].id}/drag`)
+        Rails.ajax({
+          url: `/kanbans/${el.dataset.kanbanid}/columns/${state.columns[evt.moved.newIndex].id}/drag`,
+          // url: `/kanbans/2/columns/${state.columns[evt.moved.newIndex].id}/drag`,
+          type: 'PUT',
+          data,
+          dataType: 'json'
+          // success: result => {
+          //   // commit("UPDATE_COLUMNS", result);            
+          //   console.log(result);
+          // },
+          // error: error => {
+          //   // console.log(error);            
+          // }
+        });
+      },
     fetchColumn({ commit }, kanbanid){
       // console.log("kanbanid:" + kanbanid);
       Rails.ajax({
